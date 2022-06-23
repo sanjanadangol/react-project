4c4e9df64c469727b2d5af847ebac09cc81f5089
sanjanadangol <sanjana.dangol@deerwalk.edu.np>
2022-06-23 10:47:44 +0545

    Final Commit

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,92 +1,70 @@
-// import React from 'react'
-// import './Register.css'
-
-// const Register=()=>{
-  
-//     return (
-//           <div className="container">
-//             <div className='row'>
-//                 <div className='col-5'></div>
-//                 <div className='col-5'>
-//                 <form>
-                
-
-//   <div class="form-group">
-//     <label for="exampleInputFirstName1">First name</label>
-    
-//     {<input type="fname" class="form-control" id="exampleInputFirstName" placeholder="Enter first name "/> }
-//     {/* <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> */}
-//   </div>
-//   <div class="form-group">
-//     <label for="exampleInputLastName1">Last name</label>
-//     <input type="lname" class="form-control" id="exampleInputLasttName1"  placeholder="Enter last name"/>
-//     {/* <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> */}
-//   </div>
-//   <div class="form-group">
-//     <label for="exampleInputEmail1">Email address</label>
-//     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-//     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-//   </div>
-  
-//   <div class="form-group">
-//     <label for="exampleInputPassword1">Password</label>
-//     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-//   </div>
-//   <div class="form-check">
-//     <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-//     <label class="form-check-label" for="exampleCheck1">Check me out</label>
-//   </div>
-//   <button type="Register" class="btn btn-primary">Submit</button>
-// </form>
-//                 </div>
-//             </div>
-//         </div>
-       
-//     )
-// }
-
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import React from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import axios from "axios";
 
 const Register = () =>{
+  const [email, setEmail] = useState();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+
+  const onChangeUsername = (e) => {
+		setUsername(e.target.value);
+	};
+
+	const onChangeEmail = (e) => {
+		setEmail(e.target.value);
+	};
+
+	const onChangePassword = (e) => {
+		setPassword(e.target.value);
+	};
+  const submitHandler = async (e) => {
+		e.preventDefault();
+    console.log(username)
+    await axios
+    .post("/api/auth/local/register", {
+      username,
+      email,
+      password,
+    })
+    .then((response) => {
+      console.log(response);
+      navigate("/");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+	};
   return (
     <>
       <Container>
-        <Row>
-          <Col lg={4} md={6} sm={12}>
+        <Row className="d-flex justify-content-center ">
+          <Col lg={4} md={6} sm={12} className="px-5 py-3 shadow">
             <Form>
-            <Form.Group controlId="formBasicFirstName">
-                <Form.Label>First name </Form.Label>
-                <Form.Control type="fname" placeholder="Enter first name" />
+            <Form.Group controlId="formBasicFirstName" className="mb-4">
+                <Form.Label>User name </Form.Label>
+                <Form.Control type="fname" placeholder="Enter first name" onChange={onChangeUsername}/>
               
               </Form.Group>
 
-              <Form.Group controlId="formBasicLastName">
-                <Form.Label>Last name </Form.Label>
-                <Form.Control type="lname" placeholder="Enter last name" />
-              
-              </Form.Group>
-
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="formBasicEmail" className="mb-4">
                 <Form.Label>Email address </Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" onChange={onChangeEmail} />
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
                 </Form.Text>
               </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
+              <Form.Group controlId="formBasicPassword" className="mb-4">
                 <Form.Label>Password </Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" onChange={onChangePassword}/>
               </Form.Group>
 
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" placeholder="Check me out" />
-              </Form.Group>
 
-              <Button variant='primary btn-block' type="submit">
+              <Button variant='primary btn-block' type="submit" onClick={submitHandler}>
                 Submit
               </Button>
 
@@ -97,7 +75,6 @@ const Register = () =>{
             </Form>
           </Col>
 
-          <Col lg={8} md={6} sm={12}></Col>
         </Row>
       </Container>
     </>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 
+axios.defaults.baseURL = process.env.REACT_APP_BASE_API_URL;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
